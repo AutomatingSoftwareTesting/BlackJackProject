648bae60ff9672ed039d9355eeb1e62a93a77869
Jason Caldwell <jdcald13@gmail.com>
2017-10-14 20:02:49 -0600

    Complete Hand test

--- tests/com/skilldistillery/cards/blackjack/tests/HandTest.java
+++ tests/com/skilldistillery/cards/blackjack/tests/HandTest.java
@@ -2,7 +2,6 @@ package com.skilldistillery.cards.blackjack.tests;
 
 import static org.junit.Assert.assertEquals;
 
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,24 +13,48 @@ import com.skilldistillery.cards.blackjack.Suit;
 
 public class HandTest {
 	Hand h;
-//	Card c1 = new Card(Rank.ACE, Suit.SPADES);
-//	Card c2 = new Card(Rank.TEN, Suit.SPADES);
+	Hand blackjack;
+	Hand mc;
 	
 	@Before
 	public void setUp() throws Exception {
 		h = new Hand();
-		h.addCard(new Card(Rank.ACE, Suit.SPADES));
-		h.addCard(new Card(Rank.TEN, Suit.SPADES));
+		h.addCard(new Card(Rank.KING, Suit.SPADES));
+		h.addCard(new Card(Rank.QUEEN, Suit.CLUBS));
+		
+		blackjack = new Hand();
+		blackjack.addCard(new Card(Rank.ACE, Suit.SPADES));
+		blackjack.addCard(new Card(Rank.TEN, Suit.SPADES));
+		
+		mc = new Hand();
+		mc.addCard(new Card(Rank.TWO, Suit.SPADES));
+		mc.addCard(new Card(Rank.SEVEN, Suit.HEARTS));
+		mc.addCard(new Card(Rank.FOUR, Suit.DIAMONDS));
+		mc.addCard(new Card(Rank.FIVE, Suit.CLUBS));
+		mc.addCard(new Card(Rank.TWO, Suit.CLUBS));
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		h = null;
+		blackjack = null;
 	}
 
 	@Test
-	public void test_hand_value() {
-		assertEquals(21, h.getValueOfHand());
+	public void test_hand() {
+		assertEquals("[K♠, Q♣]", h.getHand().toString());
+		assertEquals(20, h.getValueOfHand());
+	}
+	
+	@Test
+	public void test_many_cards_in_hand() {
+		assertEquals("[2♠, 7❤, 4♦, 5♣, 2♣]", mc.getHand().toString());
+		assertEquals(20, mc.getValueOfHand());
+	}
+	
+	@Test
+	public void test_blackjack() {
+		assertEquals(21, blackjack.getValueOfHand());
 	}
 
 }
